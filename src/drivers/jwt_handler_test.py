6809758c7d5df846c1b06b9f5751cb7576c4d811
FredVenturin <frederico.venturin@gmail.com>
2026-02-04 14:35:49 -0300

    feat implementing jwt handler

---
+++ src/drivers/jwt_handler_test.py
@@ -0,0 +1,16 @@
+from .jwt_handler import JwtHandler
+
+def test_jwt_handler():
+    jwt_handler = JwtHandler()
+    body = {
+        "username": "Ola",
+        "aqui": 'aquiiiii',
+        "lalala": ""
+    }
+
+    token = jwt_handler.create_jwt_token(body)
+    token_informations = jwt_handler.decode_jwt_token(token)
+
+    assert token is not None
+    assert isinstance(token,str)
+    assert token_informations["username"] == body["username"]
