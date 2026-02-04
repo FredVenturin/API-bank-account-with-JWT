bbfe551c09fe8198d9a61290c858bccba40193de
FredVenturin <frederico.venturin@gmail.com>
2026-02-04 10:47:25 -0300

    feat: implementing user repository

---
+++ src/models/repositories/user_repository_test.py
@@ -0,0 +1,17 @@
+from .user_repository import UserRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+def test_repository():
+    db_connection_handler.connect()
+    conn = db_connection_handler.get_connection()
+    repo = UserRepository(conn)
+
+    username = "Bob Esponja"
+    password = "123Rocket!"
+
+    repo.registry_user(username, password)
+    user = repo.get_user_by_username(username)
+    print()
+    print(user)
+    
+    
